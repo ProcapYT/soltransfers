c5e064e8b1c6175d86ff5e659bf225a11d0d8e70
apedrera <apedrera@gmail.com>
2025-04-16 19:21:35 +0200

    adding some logging

--- scripts/seedPhraseToPrivateKey.js
+++ scripts/seedPhraseToPrivateKey.js
@@ -5,11 +5,14 @@ import { Keypair } from "@solana/web3.js";
 import { input } from "./privateKeyEncoder.js";
 import sc from "samcolors";
 
-const seedPhrase = await input("Twelve word seed phrase: ");
 const derivationPath = "m/44'/501'/0'/0'";  // Phantom uses this
+console.log("Using derivation path", sc.yellow(derivationPath));
+
+const seedPhrase = await input("Twelve word seed phrase: ");
 
 const seed = await bip39.mnemonicToSeed(seedPhrase);
 const derivedSeed = ed25519.derivePath(derivationPath, seed.toString("hex")).key;
 const keypair = Keypair.fromSeed(derivedSeed);
+
 console.log(sc.blue("Private Key (Base58):"), bs58.encode(keypair.secretKey));
 console.log(sc.blue("Public Key (Address):"), keypair.publicKey.toBase58());
